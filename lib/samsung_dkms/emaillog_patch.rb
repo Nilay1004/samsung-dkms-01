f151ef568a535da6eb040f0e87cc2cd9c8becc05
Nilay1004 <nilaykujur@gmail.com>
2024-08-02 05:33:45 +0000

    Update emaillog_patch.rb

--- lib/samsung_dkms/emaillog_patch.rb
+++ lib/samsung_dkms/emaillog_patch.rb
@@ -5,8 +5,6 @@
 
 
 module SamsungDkms::EmailLogPatch
-  before_save :encrypt_to_address
-  after_initialize :decrypt_to_address
 
   def to_address
     @decrypted_to_address ||= PIIEncryption.decrypt_email(read_attribute(:to_address))
@@ -16,20 +14,5 @@ module SamsungDkms::EmailLogPatch
     @decrypted_to_address = value
     encrypted_to_address = PIIEncryption.encrypt_email(value)
     write_attribute(:to_address, encrypted_to_address)
-  end
-
-  private
-
-  def encrypt_to_address
-    if @decrypted_to_address.present?
-      encrypted_to_address = PIIEncryption.encrypt_email(@decrypted_to_address)
-      write_attribute(:to_address, encrypted_to_address)
-    end
-  end
-
-  def decrypt_to_address
-    if read_attribute(:to_address).present?
-      @decrypted_to_address = PIIEncryption.decrypt_email(read_attribute(:to_address))
-    end
   end
 end
