de87f9dbe45a7d966b7507cc68168de2e87a62de
Nilay1004 <nilaykujur@gmail.com>
2024-08-13 04:56:22 +0000

    commit

--- plugin.rb
+++ plugin.rb
@@ -34,17 +34,17 @@ after_initialize do
   require_relative 'lib/extensions/sessioncontroller_extension'
   require_relative 'lib/extensions/emailvalidator_extension'
   require_relative 'lib/extensions/useremail_extension'
+  require_relative "lib/extensions/emailtoken_extension"
 
   require_relative "lib/samsung_dkms/user_patch"
   require_relative "lib/samsung_dkms/emaillog_patch"
   require_relative "lib/samsung_dkms/skippedemaillog_patch"
-  require_relative "lib/samsung_dkms/emailtoken_patch"
+  
 
   reloadable_patch do |plugin|
     User.prepend(SamsungDkms::UserPatch)
     EmailLog.prepend(SamsungDkms::EmailLogPatch)
     SkippedEmailLog.prepend(SamsungDkms::SkippedEmailLogPatch)
-    EmailToken.prepend(SamsungDkms::EmailTokenPatch)
   end
 end
 
