4a165529774304822c3e61802f17716cd227e593
Nilay1004 <nilaykujur@gmail.com>
2024-08-20 05:03:24 +0000

    Update plugin.rb

--- plugin.rb
+++ plugin.rb
@@ -35,16 +35,19 @@ after_initialize do
   require_relative 'lib/extensions/emailvalidator_extension'
   require_relative 'lib/extensions/invite_extension'
   require_relative 'lib/extensions/sessioncontroller_extension'
-  require_relative 'lib/extensions/skippedemaillog_extension'
-  require_relative 'lib/extensions/user_extension'
+#  require_relative 'lib/extensions/skippedemaillog_extension'
+#  require_relative 'lib/extensions/user_extension'
   require_relative 'lib/extensions/useremail_extension'
 
   
   require_relative "lib/samsung_dkms/emaillog_patch"
-  
+  require_relative 'lib/samsung_dkms/skippedemaillog_patch'
+  require_relative 'lib/samsung_dkms/user_patch'  
 
   reloadable_patch do |plugin|
     EmailLog.prepend(SamsungDkms::EmailLogPatch)
+    SkippedEmailLog.prepend(SamsungDkms::SkippedEmailLogPatch)
+    User.prepend(SamsungDkms::UserPatch)
   end
 end
 
